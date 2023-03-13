32315e6bca76708a23d4a7fd1751bbde4bdac852
Aleff Rodrigues <aleff2594@gmail.com>
2023-03-13 17:08:24 +0000

    feat: adding the flux to register new Appointment

--- src/main/java/pt/solutions/af/appointment/model/AppointmentStatus.java
+++ src/main/java/pt/solutions/af/appointment/model/AppointmentStatus.java
@@ -1,11 +1,28 @@
 package pt.solutions.af.appointment.model;
 
-public enum AppointmentStatus {
-    SCHEDULED,
-    CONFIRMED,
-    FINISHED,
-    INVOICED,
-    CANCELLED,
-    DENIED,
-    EXCHANGE_REQUESTED
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AppointmentStatus implements Comparable<AppointmentStatus> {
+
+    private AppointmentStatusEnum status;
+    private ZonedDateTime when;
+
+    public static AppointmentStatus ofScheduled() {
+        return new AppointmentStatus(AppointmentStatusEnum.SCHEDULED, ZonedDateTime.now(ZoneId.of("UTC")));
+    }
+
+    @Override
+    public int compareTo(AppointmentStatus o) {
+        return this.when.compareTo(o.when);
+    }
 }
---
+++ src/main/java/pt/solutions/af/appointment/model/AppointmentStatusEnum.java
@@ -0,0 +1,11 @@
+package pt.solutions.af.appointment.model;
+
+public enum AppointmentStatusEnum {
+    SCHEDULED,
+    CONFIRMED,
+    FINISHED,
+    INVOICED,
+    CANCELLED,
+    DENIED,
+    EXCHANGE_REQUESTED
+}
